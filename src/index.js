67eec849792cd02d8aae3e7e4a7853e2ec0b0337
wenbo <690469465@qq.com>
2019-03-13 17:29:25 +0800

    updata

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// // var a = 1;
+// // console.log(a)
+// var a = "Mr.xue";
+// // window.onload = function () {
+// //     console.log(a)
+// // }
+// // {
+// //     var a = "薛"
+// //
+// // }
+// // console.log(a)
+// {
+//     let a = "薛"
+// }
+// //let/const会创建一个块级作用域，这里外部的console.log(x)拿不到前面2个块级作用域声明的let；
+// console.log(a)
+// for (var i=0; i<10;i++) {
+//     console.log("循环体中：" +i)
+// }
+// console.log("循环体外：" +i)
+// for (let i=0; i<10;i++) {
+//     console.log("循环体中：" +i)
+// }
+// console.log("循环体外：" +i)
+const a = "Mr.xue";
